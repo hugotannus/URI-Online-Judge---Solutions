82b07ad8ca64cfc397ad79495327dd70e9271909
Hugo Cabral Tannús <hctannus@gmail.com>
2017-04-15 17:55:36 -0300

    Ruby solution for problem #1281 - "Going to the Market".

---
+++ Estruturas/1281-market.rb
@@ -0,0 +1,16 @@
+for c in 1..(gets.to_i)
+  dict = {}
+  spent = 0.0
+
+  for i in 1..(gets.to_i)
+    t = (gets).split
+    dict[t.first] = t.last.to_f
+  end
+
+  for i in 1..(gets.to_i)
+    t = (gets).split
+    spent += dict[t.first] * t.last.to_i
+  end
+
+  puts "R$ #{'%.2f' % spent.round(2)}"
+end
